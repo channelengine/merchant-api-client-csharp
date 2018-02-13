afae82ce5ca98caf181c78b062ea62330f0ab199
Christiaan de Ridder <ik@christiaanderidder.nl>
2018-02-13 14:23:47 +0100

    Generate version 2.5.0

--- build.bat
+++ build.bat
@@ -6,12 +6,12 @@
 SET CSCPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319
 
 if not exist ".\nuget.exe" powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', '.\nuget.exe')"
-.\nuget.exe install src\ChannelEngine.Api.Client\packages.config -o packages
+.\nuget.exe install src\ChannelEngine.Merchant.ApiClient\packages.config -o packages
 
 if not exist ".\bin" mkdir bin
 
 copy packages\Newtonsoft.Json.10.0.3\lib\net45\Newtonsoft.Json.dll bin\Newtonsoft.Json.dll
 copy packages\JsonSubTypes.1.2.0\lib\net45\JsonSubTypes.dll bin\JsonSubTypes.dll
 copy packages\RestSharp.105.1.0\lib\net45\RestSharp.dll bin\RestSharp.dll
-%CSCPATH%\csc  /reference:bin\Newtonsoft.Json.dll;bin\JsonSubTypes.dll;bin\RestSharp.dll;System.ComponentModel.DataAnnotations.dll  /target:library /out:bin\ChannelEngine.Api.Client.dll /recurse:src\ChannelEngine.Api.Client\*.cs /doc:bin\ChannelEngine.Api.Client.xml
+%CSCPATH%\csc  /reference:bin\Newtonsoft.Json.dll;bin\JsonSubTypes.dll;bin\RestSharp.dll;System.ComponentModel.DataAnnotations.dll  /target:library /out:bin\ChannelEngine.Merchant.ApiClient.dll /recurse:src\ChannelEngine.Merchant.ApiClient\*.cs /doc:bin\ChannelEngine.Merchant.ApiClient.xml
 
--- build.sh
+++ build.sh
@@ -40,7 +40,7 @@ if ! type nuget &>/dev/null; then
 fi
 
 mozroots --import --sync
-${nuget_cmd} install src/ChannelEngine.Api.Client/packages.config -o packages;
+${nuget_cmd} install src/ChannelEngine.Merchant.ApiClient/packages.config -o packages;
 
 echo "[INFO] Copy DLLs to the 'bin' folder"
 mkdir -p bin;
@@ -48,15 +48,15 @@ cp packages/Newtonsoft.Json.10.0.3/lib/net45/Newtonsoft.Json.dll bin/Newtonsoft.
 cp packages/RestSharp.105.1.0/lib/net45/RestSharp.dll bin/RestSharp.dll;
 cp packages/JsonSubTypes.1.2.0/lib/net45/JsonSubTypes.dll bin/JsonSubTypes.dll
 
-echo "[INFO] Run 'mcs' to build bin/ChannelEngine.Api.Client.dll"
+echo "[INFO] Run 'mcs' to build bin/ChannelEngine.Merchant.ApiClient.dll"
 mcs -langversion:${langversion} -sdk:${sdk} -r:bin/Newtonsoft.Json.dll,bin/JsonSubTypes.dll,\
 bin/RestSharp.dll,\
 System.ComponentModel.DataAnnotations.dll,\
 System.Runtime.Serialization.dll \
 -target:library \
--out:bin/ChannelEngine.Api.Client.dll \
--recurse:'src/ChannelEngine.Api.Client/*.cs' \
--doc:bin/ChannelEngine.Api.Client.xml \
+-out:bin/ChannelEngine.Merchant.ApiClient.dll \
+-recurse:'src/ChannelEngine.Merchant.ApiClient/*.cs' \
+-doc:bin/ChannelEngine.Merchant.ApiClient.xml \
 -platform:anycpu
 
 if [ $? -ne 0 ]
@@ -64,5 +64,5 @@ then
   echo "[ERROR] Compilation failed with exit code $?"
   exit 1
 else
-  echo "[INFO] bin/ChannelEngine.Api.Client.dll was created successfully"
+  echo "[INFO] bin/ChannelEngine.Merchant.ApiClient.dll was created successfully"
 fi
